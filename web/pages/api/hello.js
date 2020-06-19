09e09145dff7d8202b22b18a77a7840af587848b
kudarap <javinczki02@gmail.com>
2020-06-19 21:14:00 +0800

    remove api page

--- web/pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ name: 'John Doe' }))
-}
