c10df9f8ecb87a7baeaed5e2719ac83eea524134
kudarap <javinczki02@gmail.com>
2020-07-10 01:43:15 +0800

    fix rarity palettes

--- web/constants/palette.js
+++ web/constants/palette.js
@@ -1,3 +1,3 @@
 export const ITEM_RARITY_RARE = '#8ec021'
-export const ITEM_RARITY_VERY_RARE = '#8ec021'
-export const ITEM_RARITY_ULTRA_RARE = '#8ec021'
+export const ITEM_RARITY_VERY_RARE = '#a77338'
+export const ITEM_RARITY_ULTRA_RARE = '#E2AC3F'
