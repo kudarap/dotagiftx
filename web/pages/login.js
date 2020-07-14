dd28cad1dcebf89d3c6f87c754ad2c74e1d5e321
kudarap <javinczki02@gmail.com>
2020-07-14 18:47:52 +0800

    update title and login page content

--- web/pages/login.js
+++ web/pages/login.js
@@ -12,7 +12,7 @@ import { set as setAuth } from '@/service/auth'
 
 const useStyles = makeStyles(theme => ({
   main: {
-    marginTop: theme.spacing(10),
+    marginTop: theme.spacing(6),
   },
 }))
 
@@ -49,16 +49,20 @@ export default function Login() {
 
       <main className={classes.main}>
         <Container>
-          <Typography variant="h6" component="h1">
-            Logging into DotagiftX allows you to access additional features.
+          <Typography variant="h5" component="h1" gutterBottom>
+            Signing in to <strong>DotagiftX</strong> allows you to access additional features.
           </Typography>
-          <Typography>
-            To provide a better service we will fetch public information about your account from the
-            Steam Web API <em>(this includes steamid, profile name, and avatar)</em>.
-          </Typography>
+          {/*<Typography>*/}
+          {/*  <ul>*/}
+          {/*    <li>Item Listing</li>*/}
+          {/*    <li>Reservation</li>*/}
+          {/*  </ul>*/}
+          {/*</Typography>*/}
           <br />
+
           <Typography>This website is not affiliated with Valve Corporation or Steam.</Typography>
           <br />
+
           <Button
             disabled={loading}
             startIcon={loading ? <CircularProgress color="secondary" size={22} /> : <SteamIcon />}
@@ -70,7 +74,13 @@ export default function Login() {
           {error && <Typography color="error">{error.message}</Typography>}
           <Typography />
           <br />
-          <Typography>We use cookies to keep your signed in session active.</Typography>
+          <br />
+
+          <Typography color="textSecondary" variant="body2">
+            By signing in, We ask for public information about your account from the Steam Web API
+            this includes (<em>steam id, profile name, and avatar image</em>) and use cookies to
+            keep your signed in session active.
+          </Typography>
         </Container>
       </main>
 
