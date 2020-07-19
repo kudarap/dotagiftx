56dbfefe5adf954bd5fb4c82f334222877c4eec0
kudarap <javinczki02@gmail.com>
2020-07-19 14:55:06 +0800

    search page now fully SSR

--- web/pages/search.js
+++ web/pages/search.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import useSWR from 'swr'
-import querystring from 'querystring'
-import { useRouter } from 'next/router'
 import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import LinearProgress from '@material-ui/core/LinearProgress'
+import { catalogSearch } from '@/service/api'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Container from '@/components/Container'
 import CatalogList from '@/components/CatalogList'
 import TablePagination from '@/components/TablePaginationRouter'
-import { CATALOGS, catalogSearch, fetcher } from '@/service/api'
 
 const useStyles = makeStyles(theme => ({
   main: {
@@ -25,19 +22,14 @@ const useStyles = makeStyles(theme => ({
 
 const defaultFilter = { sort: 'created_at:desc', page: 1 }
 
-export default function Search({ catalogs: items }) {
+export default function Search({ catalogs: items, filter: propFilter }) {
   const classes = useStyles()
 
-  const router = useRouter()
-  const { query } = router
-  query.page = Number(query.page || 1)
-  const [filter, setFilter] = React.useState({ ...defaultFilter, ...query })
+  const [filter, setFilter] = React.useState(propFilter)
 
-  // const { data: items, error } = useSWR([CATALOGS, filter], fetcher, { initialData })
-  // React.useEffect(() => {
-  //   setFilter({ ...filter, ...query })
-  // }, [query])
-  const error = null
+  React.useEffect(() => {
+    setFilter({ ...filter, ...propFilter })
+  }, [propFilter])
 
   const handlePageChange = (e, page) => {
     const f = { ...filter, page }
@@ -55,15 +47,14 @@ export default function Search({ catalogs: items }) {
           {filter.q && (
             <>
               <Typography component="h1" variant="h6">
-                Results for &quot;{filter.q}&quot;
+                {items && items.total_count} results for &quot;{filter.q}&quot;
               </Typography>
               <br />
             </>
           )}
 
-          {error && <div>failed to load</div>}
           {!items && <LinearProgress color="secondary" />}
-          {!error && items && (
+          {items && (
             <div>
               <CatalogList items={items.data} />
               <TablePagination
@@ -85,14 +76,18 @@ export default function Search({ catalogs: items }) {
 }
 Search.propTypes = {
   catalogs: PropTypes.object.isRequired,
+  filter: PropTypes.object.isRequired,
 }
 
 // This gets called on every request
 export async function getServerSideProps({ query }) {
-  const f = { ...defaultFilter, ...query }
+  const filter = { ...defaultFilter, ...query }
+  filter.page = Number(query.page || 1)
+
   return {
     props: {
-      catalogs: await catalogSearch(f),
+      catalogs: await catalogSearch(filter),
+      filter,
     },
   }
 }
