6c59b477bead867ea16e6bad7643a3cfe5b1fd44
kudarap <javinczki02@gmail.com>
2020-08-18 14:16:16 +0800

    remove unused deps on footer component

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import MuiLink from '@material-ui/core/Link'
 import Divider from '@material-ui/core/Divider'
 import Typography from '@material-ui/core/Typography'
 import Container from '@/components/Container'
