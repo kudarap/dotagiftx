044913ac0b9b8d45fd23c6c4b3f1fa49e2e21d97
kudarap <javinczki02@gmail.com>
2020-09-20 07:26:47 +0800

    fixes jpe image ext

--- fixes/fixes.go
+++ fixes/fixes.go
@@ -2,7 +2,9 @@ package fixes
 
 import (
 	"context"
+	"log"
 	"math/rand"
+	"strings"
 
 	"github.com/kudarap/dotagiftx/core"
 )
@@ -34,6 +36,29 @@ func GenerateFakeMarket(
 			}
 			auc := core.AuthToContext(ctx, &core.Auth{UserID: user.ID})
 			marketSvc.Create(auc, m)
+		}
+	}
+}
+
+func ImageFileExtJPG(itemStg core.ItemStorage, userStg core.UserStorage) {
+	const suf = ".jpe"
+	const newSuf = ".jpg"
+
+	items, _ := itemStg.Find(core.FindOpts{})
+	for _, ii := range items {
+		if strings.HasSuffix(ii.Image, suf) {
+			ii.Image = strings.Replace(ii.Image, suf, newSuf, 1)
+			log.Println("new item image", ii.Image)
+			itemStg.Update(&ii)
+		}
+	}
+
+	users, _ := userStg.Find(core.FindOpts{})
+	for _, uu := range users {
+		if strings.HasSuffix(uu.Avatar, suf) {
+			uu.Avatar = strings.Replace(uu.Avatar, suf, newSuf, 1)
+			log.Println("new user avatr", uu.Avatar)
+			userStg.Update(&uu)
 		}
 	}
 }
