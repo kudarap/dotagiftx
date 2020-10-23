493276438312dfd1e85270a503fc923bce1048fb
kudarap <javinczki02@gmail.com>
2020-10-23 11:08:47 +0800

    inventory parser now using cache

--- scripts/inventory-parser.go
+++ scripts/inventory-parser.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 
 	jsoniter "github.com/json-iterator/go"
 )
 
-var json = jsoniter.ConfigFastest
+var fastjson = jsoniter.ConfigFastest
 
 type Item struct {
 	ClassID     string      `json:"classid"`
@@ -41,18 +46,42 @@ type Inventory struct {
 	Items map[string]Item `json:"rgDescriptions"`
 }
 
-func main() {
-	fmt.Println("hello")
+const (
+	steamCDN          = "https://steamcommunity-a.akamaihd.net/economy/image/"
+	steamInventoryAPI = "https://steamcommunity.com/profiles/%s/inventory/json/570/2?"
 
-	// read file
-	data, err := ioutil.ReadFile("./inventory.json")
+	filenameFmt = "dgx-inventory-%s.json"
+)
+
+func main() {
+	filterDescPtr := flag.String("filter", "2019", "description filter")
+	filterTypePtr := flag.String("type", "Mythical Bundle", "type filter")
+	flag.Parse()
+
+	ids := flag.Args()
+	if len(ids) == 0 {
+		fmt.Println("steam id required")
+		return
+	}
+	steamID := ids[0]
+
+	// Read file cache if exist, else download
+	cacheSrc := getSource(steamID)
+	if _, err := os.Stat(cacheSrc); os.IsNotExist(err) {
+		if err := dlCache(steamID); err != nil {
+			fmt.Println("could not dl cache", err)
+			return
+		}
+	}
+
+	data, err := ioutil.ReadFile(cacheSrc)
 	if err != nil {
 		fmt.Println("could not read file:", err)
 		return
 	}
 
 	var inv Inventory
-	if err := json.Unmarshal(data, &inv); err != nil {
+	if err := fastjson.Unmarshal(data, &inv); err != nil {
 		fmt.Println("could not parse json:", err)
 	}
 	fmt.Println("parsed", len(inv.Items))
@@ -60,13 +89,13 @@ func main() {
 	const filter = "International 2019"
 	items := map[string]Item{}
 	for _, ii := range inv.Items {
-		//if ii.Type != "Mythical Bundle" {
-		if !strings.Contains(ii.Type, "Mythical Bundle") {
+		if !strings.Contains(strings.ToLower(ii.Type), strings.ToLower(*filterTypePtr)) {
 			continue
 		}
 
 		desc, hero := ii.stringifyDesc()
-		if !strings.Contains(strings.ToLower(desc), strings.ToLower(filter)) {
+		if !strings.Contains(strings.ToLower(desc), strings.ToLower(*filterDescPtr)) ||
+			hero == "" {
 			continue
 		}
 
@@ -83,10 +112,34 @@ func main() {
 	for _, ii := range items {
 		fmt.Println(ii.Name)
 		fmt.Println(ii.Hero)
-		fmt.Println(ii.Image)
+		fmt.Println(steamCDN + ii.Image)
 		//fmt.Println(ii.Description)
 		fmt.Println(strings.Repeat("-", 55))
 	}
 
 	fmt.Println("total", len(items))
+}
+
+func getSource(steamID string) string {
+	return filepath.Join(os.TempDir(), fmt.Sprintf(filenameFmt, steamID))
+}
+
+func dlCache(steamID string) error {
+	url := fmt.Sprintf(steamInventoryAPI, steamID)
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	filepath := getSource(steamID)
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
