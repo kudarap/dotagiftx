ba8fb90e21f6e5f5ad3cf599a75c3709bfcc0448
kudarap <javinczki02@gmail.com>
2020-11-09 22:30:00 +0800

    fixes constrast very-rare color code

--- web/constants/palette.js
+++ web/constants/palette.js
@@ -1,6 +1,6 @@
 export const ITEM_RARITY_RARE = '#8ec021'
-export const ITEM_RARITY_VERY_RARE = '#a77338'
-export const ITEM_RARITY_ULTRA_RARE = '#E2AC3F'
+export const ITEM_RARITY_VERY_RARE = '#fb8600'
+export const ITEM_RARITY_ULTRA_RARE = '#e2ac3f'
 
 export const itemRarityColorMap = {
   rare: ITEM_RARITY_RARE,
