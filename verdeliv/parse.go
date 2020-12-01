ae49a31fa4568934520e87a1ec559742e8b94c56
kudarap <javinczki02@gmail.com>
2020-12-01 13:19:35 +0800

    WIP: added verdliv package

---
+++ verdeliv/parse.go
@@ -0,0 +1,16 @@
+package verdeliv
+
+/*
+
+
+
+Take inventory json files input
+	- support multiple json for paginated large inventory
+
+Parse steam inventory
+	- detect malformed json
+	- detect private inventory
+	- total number of items
+	-
+
+*/
