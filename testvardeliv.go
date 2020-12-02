a5b9b19c2f9a2fd2e48080ecc54255e263f1b3d0
kudarap <javinczki02@gmail.com>
2020-12-02 16:14:18 +0800

    WIP: added test case from unwrapped gift

--- testvardeliv.go
+++ testvardeliv.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 
+	"github.com/kudarap/dotagiftx/core"
 	"github.com/kudarap/dotagiftx/verdeliv"
 )
 
@@ -24,13 +29,14 @@ func main() {
 		{"kudarap", "76561198436826874", "Fowl Omen"},
 		{"kudarap", "76561198170463425", "Cinder Sensei"},
 		{"kudarap", "76561198872556187", "Grim Destiny"},
-		// Chiw
 		{"Berserk", "76561198042690669", "Allure of the Faeshade Flower"},
 		{"Berserk", "76561198256569879", "Endless Night"},
 		{"Berserk", "76561198139657329", "Glimmer of the Sacred Hunt"},
 		{"Dark Knight", "76561198116319576", "Legends of Darkheart Pursuit"},
 		{"kudarap", "76561198042690669", "Scorched Amber"},
 		{"kudarap", "76561198872556187", "Tales of the Windward Rogue"},
+		{"kudarap", "76561198203634725", "Adornments of the Jade Emissary"},
+		{"gippeum", "76561198088587178", "Elements of the Endless Plane"},
 	}
 
 	for _, param := range params {
@@ -40,12 +46,36 @@ func main() {
 			continue
 		}
 
-		fmt.Println(fmt.Sprintf("%s check %s hit: %d", param.persona, param.item, len(res)))
-		for _, rr := range res {
-			fmt.Println(rr.Name)
+		fmt.Println(strings.Repeat("-", 70))
+		fmt.Println(fmt.Sprintf("%s -> %s (%s)", param.persona, param.steamID, param.item))
+		fmt.Println(strings.Repeat("-", 70))
+		fmt.Println("Found:", len(res))
+		if len(res) != 0 {
+			r := res[0]
+			fmt.Println("GiftFrom:", r.GiftFrom)
+			fmt.Println("DateReceived:", r.DateReceived)
+			fmt.Println("Dedication:", r.Dedication)
 		}
 
 		fmt.Println("")
 	}
 
+}
+
+func getDelivered() ([]core.Market, error) {
+	resp, err := http.Get("https://api.dotagiftx.com/markets?sort=updated_at:desc&limit=50&status=400")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data := struct {
+		Data []core.Market
+	}{}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err := json.Unmarshal(b, &data); err != nil {
+		return nil, err
+	}
+
+	return data.Data, nil
 }
--- verdeliv/verdeliv.go
+++ verdeliv/verdeliv.go
@@ -69,10 +69,14 @@ func Verify(sellerPersona, buyerSteamID, itemName string) ([]flatInventory, erro
 
 	var fi []flatInventory
 	for _, inv := range res {
+		// Checking against seller persona name might not be accurate since
+		// buyer can clear gift information that's why it need to snapshot buyer
+		// inventory immediately.
 		if inv.GiftFrom != sellerPersona {
 			continue
 		}
 
+		// Checks target item name from description and name field.
 		if !strings.Contains(strings.Join(inv.Descriptions, "|"), itemName) &&
 			!strings.Contains(inv.Name, itemName) {
 			continue
