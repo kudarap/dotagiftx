e4d04b6914fb12275d6fad36742a1f9103f27c06
kudarap <javinczki02@gmail.com>
2020-12-07 13:03:31 +0800

    change contact buyer buton

--- web/components/MarketList.js
+++ web/components/MarketList.js
@@ -303,14 +303,14 @@ export default function MarketList({ offers, buyOrders, error, pagination }) {
                               </Button>
                             </div>
                           ) : (
-                            <Button
+                            <BuyButton
                               // Check for redacted user and disabled them for opening the dialog.
                               disabled={!market.user.id}
                               color="primary"
                               variant="contained"
                               onClick={() => handleContactClick(idx)}>
                               Contact Buyer
-                            </Button>
+                            </BuyButton>
                           )}
                         </TableCell>
                       </>
