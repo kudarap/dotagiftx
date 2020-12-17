60613f251d68e1ce51201a48f02fb70494be9caf
kudarap <javinczki02@gmail.com>
2020-12-18 06:21:31 +0800

    update new dota hero bg image

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -9,7 +9,7 @@ import { APP_FOOTER_HEIGHT } from '@/constants/app'
 
 // Stole from SteamDB dota 2 page footer.
 const dotaHeroImage =
-  'https://cdn.cloudflare.steamstatic.com/steam/apps/570/library_hero.jpg?t=1605830961'
+  'https://cdn.cloudflare.steamstatic.com/steam/apps/570/library_hero.jpg?t=1608240991'
 
 // const heroImage = '/assets/bg_hero.png'
 
