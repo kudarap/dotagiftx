66ae86b64edea0edbe40d23d352b61e9bc36cb94
kudarap <javinczki02@gmail.com>
2021-01-13 03:41:07 +0800

    WIP: inventory checker package

--- invchecker/invechecker.go
+++ invchecker/invechecker.go
@@ -1,1 +1,17 @@
 package invchecker
+
+import "github.com/kudarap/dotagiftx/core"
+
+type AssetIDs string
+
+type InventoryCheck struct {
+}
+
+func ItemExist(steamID, itemName string) ([]AssetIDs, error) {
+	panic("implemeneme")
+}
+
+func ImportItems() ([]core.Item, error) {
+	panic("implemeneme")
+
+}
