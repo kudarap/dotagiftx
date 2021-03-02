f35c2b75fd7dd2dc27d037ff86501cb781648d78
kudarap <javinczki02@gmail.com>
2021-03-03 05:04:26 +0800

    invalidate auth_key

--- web/components/Root.js
+++ web/components/Root.js
@@ -5,25 +5,12 @@ import useMediaQuery from '@material-ui/core/useMediaQuery'
 import * as Auth from '@/service/auth'
 import Theme from '@/components/Theme'
 import AppContext from '@/components/AppContext'
-import { useRouter } from 'next/router'
 
 function Root({ children }) {
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down('xs'))
   const currentAuth = Auth.get()
   const isLoggedIn = Auth.isOk()
-
-  // Checks for new auth field steam_id.
-  // need to re-authenticate to get new field.
-  const router = useRouter()
-  React.useEffect(() => {
-    if (!isLoggedIn || currentAuth.steam_id) {
-      return
-    }
-
-    Auth.clear()
-    router.push('/login')
-  }, [currentAuth])
 
   return (
     <AppContext.Provider value={{ isMobile, currentAuth, isLoggedIn }}>
