e97b8e46e599e077086490fc4f00dfdce08b3678
kudarap <javinczki02@gmail.com>
2021-03-12 18:41:43 +0800

    simplify jobs registration, now using array of jobs

--- worker/worker.go
+++ worker/worker.go
@@ -8,14 +8,16 @@ import (
 	"time"
 )
 
-const defaultJobInterval = time.Second * 5
-
 // JobID represents identification for a Job.
 type JobID string
 
 // Job provides process and information for the job.
 type Job interface {
 	ID() string
+	Task
+}
+
+type Task interface {
 	Run(context.Context) error
 	Interval() time.Duration
 }
@@ -24,33 +26,29 @@ type Job interface {
 type Worker struct {
 	wg    sync.WaitGroup
 	quit  chan struct{}
-	queue chan JobID
-	jobs  map[JobID]Job
+	queue chan Job
+	jobs  []Job
 }
 
 // New create new instance of a worker with a given jobs.
 func New(jobs ...Job) *Worker {
 	w := &Worker{}
-	w.queue = make(chan JobID, 1)
+	w.queue = make(chan Job, 1)
 	w.quit = make(chan struct{})
-	w.jobs = map[JobID]Job{}
-
-	for _, jj := range jobs {
-		w.jobs[JobID(jj.ID())] = jj
-	}
+	w.jobs = jobs
 
 	return w
 }
 
 // Start initiates worker to start running the jobs.
 //
-// All assigned jobs to worker will be run concurrently.
+// All assigned jobs will be run concurrently.
 func (w *Worker) Start() error {
 	w.logger("running", len(w.jobs), "jobs...")
 
 	go func() {
-		for id, _ := range w.jobs {
-			w.queueJob(id)
+		for _, jj := range w.jobs {
+			w.queueJob(jj)
 		}
 	}()
 
@@ -59,38 +57,41 @@ func (w *Worker) Start() error {
 		select {
 		case <-w.quit:
 			return nil
-		case jobID := <-w.queue:
-			go w.runner(ctx, jobID)
+		case job := <-w.queue:
+			go w.runner(ctx, job)
 		}
 	}
 }
 
-// runner process the job and will re-queue them when recurring job.
-func (w *Worker) runner(ctx context.Context, id JobID) {
-	w.wg.Add(1)
+func (w *Worker) RunOnce() {
 
-	w.logger(fmt.Sprintf("job:%s recv", id))
-	job := w.jobs[id]
-	if err := job.Run(ctx); err != nil {
-		w.logger(fmt.Sprintf("job:%s error! %s", id, err))
-	}
-
-	w.logger(fmt.Sprintf("job:%s done!", id))
-	w.wg.Done()
-
-	// Rest before next occurrence.
-	d := job.Interval()
-	if d == 0 {
-		d = defaultJobInterval
-	}
-	time.Sleep(d)
-
-	w.queueJob(id)
 }
 
-func (w *Worker) queueJob(id JobID) {
-	w.queue <- id
-	w.logger(fmt.Sprintf("job:%s queued", id))
+// runner process the job and will re-queue them when recurring job.
+func (w *Worker) runner(ctx context.Context, task Job) {
+	w.wg.Add(1)
+
+	w.logger(fmt.Sprintf("job:%s recv", task))
+	if err := task.Run(ctx); err != nil {
+		w.logger(fmt.Sprintf("job:%s error! %s", task, err))
+	}
+	w.logger(fmt.Sprintf("job:%s done!", task))
+	w.wg.Done()
+
+	// Determines if the job is run-once by interval value is zero.
+	rest := task.Interval()
+	if rest == 0 {
+		return
+	}
+	// Job that has non-zero interval value means its a recurring job
+	// and will be re-queued after its rest duration.
+	time.Sleep(rest)
+	w.queueJob(task)
+}
+
+func (w *Worker) queueJob(j Job) {
+	w.queue <- j
+	w.logger(fmt.Sprintf("job:%s queued", j))
 }
 
 // Stop will stop accepting job and wait for processing job to finish.
