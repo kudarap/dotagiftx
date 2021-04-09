be51ff1395145af811d397705feed750fb8979ce
kudarap <javinczki02@gmail.com>
2021-04-10 06:53:07 +0800

    update footer hero

--- web/constants/app.js
+++ web/constants/app.js
@@ -2,7 +2,7 @@
 
 export const APP_CACHE_PROFILE = 'profile'
 
-export const APP_FOOTER_HEIGHT = 150
+export const APP_FOOTER_HEIGHT = 250
 // total = footer height + footer margin + header height + 5th dimension, I don't where it came from.
 // total is 255px
 export const APP_FOOTER_HEIGHT_TOTAL = APP_FOOTER_HEIGHT + 40 + 50 + 15
