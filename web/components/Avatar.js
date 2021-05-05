1748829cd429bf4364260e88c1af5ab6ec113f81
kudarap <javinczki02@gmail.com>
2021-05-05 18:45:37 +0800

    fix donator glow

--- web/components/Avatar.js
+++ web/components/Avatar.js
@@ -3,21 +3,40 @@ import PropTypes from 'prop-types'
 import MuiAvatar from '@material-ui/core/Avatar'
 
 export default function Avatar(props) {
-  const { glow, style: initStyle, ...other } = props
+  const { glow, style: initStyle, src, ...other } = props
 
   let style = initStyle
   if (glow) {
     style = {
       ...style,
       border: '1px solid goldenrod',
-      animation: 'donatorglow4 12s infinite',
-      animationFillMode: 'forwards',
-      animationDelay: '3s',
-      animationTimingFunction: 'ease-in-out',
+      // animation: 'donatorglow4 12s infinite',
+      // animationFillMode: 'forwards',
+      // animationDelay: '3s',
+      // animationTimingFunction: 'ease-in-out',
     }
   }
 
-  return <MuiAvatar style={style} {...other} />
+  return (
+    <MuiAvatar style={style} {...other}>
+      <img src={src} alt="" style={{ width: '100%', height: '100%' }} />
+      {glow && (
+        <div style={{ position: 'absolute', margin: '-12%' }}>
+          <img style={{ width: '100%', height: '100%', display: 'block' }} alt="" src="/glow.png" />
+        </div>
+      )}
+    </MuiAvatar>
+  )
+
+  // return (
+  //   <div style={style}>
+  //     <img alt="" {...other} />
+  //     <div style={{ position: 'absolute', margin: -12 }}>
+  //       <img style={{ width: '100%', height: '100%', display: 'block' }} alt="" src="/glow.png" />
+  //     </div>
+  //   </div>
+  // )
+  // return <MuiAvatar style={style} {...other} />
 }
 
 Avatar.propTypes = {
