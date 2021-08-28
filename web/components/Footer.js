61827483afe3b510b14706d04479807bd13d8e61
kudarap <javinczki02@gmail.com>
2021-08-28 15:58:03 +0800

    fixes faq label link on footer

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -102,7 +102,7 @@ export default function Footer() {
           {/* </li> */}
           <li>
             <Link href="/faq" color="textSecondary">
-              FAQ
+              FAQs
             </Link>
           </li>
           <li>
