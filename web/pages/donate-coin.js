7822f7ec87ef652b1e9eeb2001c87cd5d3f72764
kudarap <javinczki02@gmail.com>
2021-09-30 21:40:04 +0800

    update donation link

--- web/pages/donate-coin.js
+++ web/pages/donate-coin.js
@@ -105,7 +105,7 @@ export default function Faq() {
             size="large"
             target="_blank"
             rel="noreferrer noopener"
-            href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=LBY7VY8PQ9D3Y&item_name=Support+DotagiftX.com&currency_code=USD">
+            href="https://www.paypal.com/donate?hosted_button_id=2788DNVV23E4J">
             Donate with PayPal
           </PaypalButton>
           <Typography color="textSecondary" style={{ marginTop: 6 }}>
