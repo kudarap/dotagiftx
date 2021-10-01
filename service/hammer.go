d0a1a69401915a6863e3e546b768080afc9f4e2d
kudarap <javinczki02@gmail.com>
2021-10-02 01:43:17 +0800

    fix hammer weilding check

--- service/hammer.go
+++ service/hammer.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/kudarap/dotagiftx/core"
 )
@@ -116,7 +117,8 @@ func (s *BanService) sunderListings(userID string, from, to core.MarketStatus) e
 }
 
 func weildingHammer(u *core.User) error {
-	if u != nil && !u.Hammer {
+	fmt.Println("weildingHammer", u.Hammer)
+	if u == nil || !u.Hammer {
 		return ErrHammerNotWeilded
 	}
 
