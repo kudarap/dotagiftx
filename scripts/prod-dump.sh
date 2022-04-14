a13a5ece6fa7df9e6ade524abfbc25c91ede7b71
kudarap <javinczki02@gmail.com>
2022-04-14 12:13:56 +0800

    fixes production dump db script

--- scripts/prod-dump.sh
+++ scripts/prod-dump.sh
@@ -5,20 +5,20 @@
 # scp root@mashdrop.com:'/opt/backups/rethink/'rethinkdb_dump_2022-01-22T03:00:03.tar.gz dump.tar.gz
 # date +%Y-%m-%d.%H:%M:%S
 
-target_db="ratatxt_dev"
+target_db="d2g_production"
 
 db_host="root@mashdrop.com"
 out_file="dump.tar.gz"
 
 dump_path="/opt/backups/rethink/"
-cur_file=`date +%Y-%m-%d`
+cur_file=`date -v '-1d' +%Y-%m-%d`
 file_name="rethinkdb_dump_${cur_file}T03:00:03"
 
 # compose file path
 target="${dump_path}${file_name}.tar.gz"
 
 # download via scp
-#scp $db_host:$target $out_file
+scp $db_host:$target $out_file
 
 # extract files
 tar -xvzf $out_file
