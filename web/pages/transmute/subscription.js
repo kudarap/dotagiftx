7e25d30ee95c14e3cfc82db028b843208d1b0541
kudarap <javinczki02@gmail.com>
2022-04-15 21:49:02 +0800

    implement live plan id for paypal subscription

--- web/pages/transmute/subscription.js
+++ web/pages/transmute/subscription.js
@@ -4,14 +4,12 @@ import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
 import { styled } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import Alert from '@mui/material/Alert'
 import Header from '@/components/Header'
 import Container from '@/components/Container'
 import Footer from '@/components/Footer'
 import AppContext from '@/components/AppContext'
-import Link from '@/components/Link'
 
-const isPaypalLive = false
+const isPaypalLive = process.env.NEXT_PUBLIC_API_URL.startsWith('https://dotagiftx.com')
 
 const subscriptions = {
   supporter: {
@@ -19,21 +17,21 @@ const subscriptions = {
     name: 'Supporter',
     features: ['Supporter Badge', 'Refresher Shard'],
     planId: 'P-616716383W896284VMJMR4CY',
-    planIdLive: 'P-616716383W896284VMJMR4CY',
+    planIdLive: 'P-0EB00258NU2523843MJMW6JY',
   },
   trader: {
     id: 'trader',
     name: 'Trader',
     features: ['Trader Badge', 'Refresher Orb'],
     planId: 'P-28V29656NC814125PMJMSDWQ',
-    planIdLive: 'P-28V29656NC814125PMJMSDWQ',
+    planIdLive: 'P-6TG171216S461482EMJMW55Q',
   },
   partner: {
     id: 'partner',
     name: 'Partner',
     features: ['Partner Badge', 'Refresher Orb', "Shopkeeper's Contract", 'Dedicated Pos-5'],
     planId: 'P-2FS77965H7642004PMJMSD6Q',
-    planIdLive: 'P-2FS77965H7642004PMJMSD6Q',
+    planIdLive: 'P-0EB00258NU2523843MJMW6JY',
   },
 }
 
