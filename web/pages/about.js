11fe10365c22ef60d10c021140098bc550c8faa1
kudarap <javinczki02@gmail.com>
2024-07-14 03:10:53 +0700

    fix about page

--- web/pages/about.js
+++ web/pages/about.js
@@ -61,7 +61,7 @@ export default function About() {
           <br />
 
           <Typography variant="h5" gutterBottom>
-            Who is behind this?
+            Who is behind it?
           </Typography>
           <Avatar src="/kudarap.jpg" style={{ width: 100, height: 100 }} />
           <Typography color="textSecondary">
