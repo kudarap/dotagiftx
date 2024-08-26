78f375eb7d1070bdfa4305f0450da195fdcfe52c
kudarap <javinczki02@gmail.com>
2024-08-27 04:07:45 +0800

    replace span uuid values to prevent high cardinality

--- rethink/span.go
+++ rethink/span.go
@@ -2,12 +2,13 @@ package rethink
 
 import (
 	"fmt"
+	"regexp"
 	"time"
 
 	r "gopkg.in/rethinkdb/rethinkdb-go.v6"
 )
 
-type spanStorage struct {
+type SpanStorage struct {
 	db *Client
 }
 
@@ -17,14 +18,21 @@ type span struct {
 	CreatedAt time.Time `db:"created_at"`
 }
 
-func NewSpan(c *Client) *spanStorage {
-	return &spanStorage{c}
+func NewSpan(c *Client) *SpanStorage {
+	return &SpanStorage{c}
 }
 
-func (s *spanStorage) Add(name string, elapsedMs int64, t time.Time) {
+func (s *SpanStorage) Add(name string, elapsedMs int64, t time.Time) {
+	name = spanCleanUUIDs(name)
 	i := span{name, elapsedMs, t}
 	_, err := s.db.insert(r.Table("span").Insert(i))
 	if err != nil {
 		fmt.Println("ERR SPAN", err)
 	}
+}
+
+var spanUUID = regexp.MustCompile(`[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`)
+
+func spanCleanUUIDs(s string) string {
+	return spanUUID.ReplaceAllString(s, "{UUID}")
 }
