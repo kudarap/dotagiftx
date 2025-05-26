7a24495668946f5d46b31fd241c71af851cee4b4
kudarap <javinczki02@gmail.com>
2025-05-26 21:35:23 +0800

    rename delivery

--- gokit/hash/md5.go
+++ gokit/hash/md5.go
@@ -3,8 +3,6 @@ package hash
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"fmt"
-	"time"
 )
 
 // MD5 returns a md5 hashed string.
@@ -13,10 +11,4 @@ func MD5(s string) string {
 	h.Write([]byte(s + Salt))
 
 	return hex.EncodeToString(h.Sum(nil))
-}
-
-// GenerateMD5 returns a generated md5 hash.
-func GenerateMD5() string {
-	t := time.Now().UnixNano()
-	return MD5(fmt.Sprintf("%d %s", t, Salt))
 }
