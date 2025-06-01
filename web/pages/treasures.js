12ff78953211ecd5fac8b553ed1057bb77de565d
kudarap <javinczki02@gmail.com>
2025-06-01 22:01:45 +0800

    increase recent treasure from 15 to 30 days

--- web/pages/treasures.js
+++ web/pages/treasures.js
@@ -11,7 +11,7 @@ import Image from 'next/image'
 import Link from '@/components/Link'
 import { APP_NAME } from '@/constants/strings'
 
-const stillNewDays = 15
+const stillNewDays = 30
 
 const treasures = [
   {
