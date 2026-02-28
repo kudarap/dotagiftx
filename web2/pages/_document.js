e223a5febfc732952d4fd82afc3c3d4d42401324
kudarap <goto@chiligarlic.com>
2026-02-28 12:13:27 +0800

    functional base page setup

--- web2/pages/_document.js
+++ web2/pages/_document.js
@@ -7,11 +7,36 @@ export default function MyDocument(props) {
   return (
     <Html lang="en">
       <Head>
+        <DocumentHeadTags {...props} />
+
+        {/* resolves dns for fast load time from other resources */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://www.googleanalytics.com" />
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="preconnect" href="https://cdn.cloudflare.steamstatic.com" />
+
         {/* PWA primary color */}
         <meta name="theme-color" content={theme.palette.primary.main} />
         <link rel="icon" href="/favicon.ico" />
         <meta name="emotion-insertion-point" content="" />
-        <DocumentHeadTags {...props} />
+
+        {process.env.NEXT_PUBLIC_GA && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments)}
+                gtag("js", new Date());
+                gtag("config", "${process.env.NEXT_PUBLIC_GA}");`,
+              }}
+            />
+          </>
+        )}
       </Head>
       <body>
         <Main />
