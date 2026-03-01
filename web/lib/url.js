5930bfc587ae4c01db9ea32e7cec91338aa4a121
kudarap <goto@chiligarlic.com>
2026-03-01 19:22:03 +0800

    fix url validation

--- web/lib/url.js
+++ web/lib/url.js
@@ -6,14 +6,10 @@ export function getQuery(s) {
 }
 
 export function isValid(s) {
-  const pattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
-    'i'
-  ) // fragment locator
-  return !!pattern.test(s)
+  try {
+    const parsed = new URL(s)
+    return parsed.protocol === 'http:' || parsed.protocol === 'https:'
+  } catch (_) {
+    return false
+  }
 }
