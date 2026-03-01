34e8141a4979b7c37abb6c0c26e89e6952d7fd9e
kudarap <goto@chiligarlic.com>
2026-03-01 19:00:58 +0800

    post item fix attempt

--- web/pages/post-item.js
+++ web/pages/post-item.js
@@ -1,24 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
-import { makeStyles } from 'tss-react/mui'
 import { APP_NAME } from '@/constants/strings'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Container from '@/components/Container'
 import MarketForm from '@/components/MarketForm'
 
-const useStyles = makeStyles()(theme => ({
-  main: {
-    [theme.breakpoints.down('md')]: {
-      marginTop: theme.spacing(2),
-    },
-    marginTop: theme.spacing(4),
-  },
-}))
-
 export default function About() {
-  const { classes } = useStyles()
-
   return (
     <>
       <Head>
@@ -28,7 +16,14 @@ export default function About() {
 
       <Header />
 
-      <main className={classes.main}>
+      <main sx={() =>(
+        {
+          [theme.breakpoints.down('md')]: {
+            marginTop: theme.spacing(2),
+          },
+          marginTop: theme.spacing(4),
+        }
+      )}>
         <Container maxWidth="sm">
           <MarketForm />
         </Container>
