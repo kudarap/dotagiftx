d86b316ba33bc4adda2738eaa4ebdb6b0bcd3dcb
kudarap <goto@chiligarlic.com>
2026-07-10 20:28:18 +0800

    allow dev api domain on nextjs images

--- web/next.config.js
+++ web/next.config.js
@@ -34,7 +34,7 @@ module.exports = {
     ]
   },
   images: {
-    domains: ['localhost', 'api.dotagiftx.com', 'nyc.api.dotagiftx.com'],
+    domains: ['localhost', 'api.dotagiftx.com', 'api.dev.dotagiftx.com'],
     minimumCacheTTL: 31556952, // 1 year
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
