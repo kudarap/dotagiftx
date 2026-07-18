1ef750696bc5f0f4d40cee97eb3a15f53ae5a330
kudarap <goto@chiligarlic.com>
2026-07-18 23:12:17 +0800

    initial fix

--- steam/steam_test.go
+++ steam/steam_test.go
@@ -23,10 +23,22 @@ func Test_cleanProfileURL(t *testing.T) {
 			"https://steamcommunity.com/profiles/76561198088587178/inventory/#570",
 			"https://steamcommunity.com/profiles/76561198088587178",
 		},
+		{
+			"https://steamcommunity.com/id/kudrap",
+			"https://steamcommunity.com/id/kudrap",
+		},
+		{
+			"https://steamcommunity.com/id/kudrap/inventory",
+			"https://steamcommunity.com/id/kudrap",
+		},
+		{
+			"https://steamcommunity.com/id/kudrap/inventory/#570",
+			"https://steamcommunity.com/id/kudrap",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.url, func(t *testing.T) {
-			if got := cleanProfileURL(tt.url); got != tt.want {
+			if got, _ := cleanProfileURL(tt.url); got != tt.want {
 				t.Errorf("got: %v, want: %v", got, tt.want)
 			}
 		})
