2b07544717cae74f16261c96f3cb238a0a6e84b5
ricomonster <2063313+ricomonster@users.noreply.github.com>
2026-08-02 23:15:06 +0800

    style: adjust image dimensions for UI consistency

--- web/components/InternalUserCard.js
+++ web/components/InternalUserCard.js
@@ -60,8 +60,8 @@ export default function InternalUserCard({ name, id, img, boons, discordURL, cre
             startIcon={
               <Image
                 src="/icon_2x.png"
-                width={60}
-                height={60}
+                width={16}
+                height={16}
                 style={{ height: 16, filter: 'brightness(10)' }}
                 alt="dgx"
               />
--- web/pages/plus.js
+++ web/pages/plus.js
@@ -248,7 +248,7 @@ export default function Plus() {
                   <Image
                     src="/assets/refresher-shard.png"
                     alt="assets/refresher-shard.png"
-                    width={48}
+                    width={66}
                     height={48}
                   />
                   <Typography>Refresher Shard</Typography>
@@ -263,7 +263,7 @@ export default function Plus() {
                   <Image
                     src="/assets/refresher-orb.png"
                     alt="assets/refresher-orb.png"
-                    width={48}
+                    width={66}
                     height={48}
                   />
                   <Typography>Refresher Orb</Typography>
@@ -275,7 +275,7 @@ export default function Plus() {
 
               <Grid item md={3} sm={4} xs={6}>
                 <Box textAlign="center" sx={{ bgcolor: 'background.paper', p: 2, borderRadius: 2 }}>
-                  <Image src="/assets/recipe.png" alt="assets/recipe.png" height={48} width={48} />
+                  <Image src="/assets/recipe.png" alt="assets/recipe.png" height={48} width={66} />
                   <Typography>Shopkeeper&apos;s Contract</Typography>
                   <Typography variant="caption" color="text.secondary">
                     Grants the ability to resell items outside your inventory
@@ -289,7 +289,7 @@ export default function Plus() {
                     src="/assets/courier.png"
                     alt="assets/courier.png"
                     height={48}
-                    width={48}
+                    width={66}
                   />
                   <Typography>Dedicated Pos-5</Typography>
                   <Typography variant="caption" color="text.secondary">
