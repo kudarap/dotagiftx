78d85e7d83ed0aa5f97631984079ebf386f7c429
kudarap <goto@chiligarlic.com>
2026-08-14 01:48:36 +0800

    add comment

--- web/e2e/pages.spec.ts
+++ web/e2e/pages.spec.ts
@@ -1,20 +1,14 @@
 import { test, expect } from '@playwright/test'
 import { DOTAGIFTX_URL } from './constants/urls'
 
-test('index', async ({ page }) => {
-  await page.goto(`${DOTAGIFTX_URL}`)
-
-  await expect(page).toHaveTitle(/Community Market/)
-})
-
+// TODO: need test page is loaded
 test('item page', async ({ page }) => {
   await page.goto(`${DOTAGIFTX_URL}/the-rat-king-chen`)
-
   await expect(page).toHaveTitle(/The Rat King/)
 })
 
+// TODO: need test page is loaded
 test('profile page', async ({ page }) => {
   await page.goto(`${DOTAGIFTX_URL}/profiles/76561198088587178`)
-
   await expect(page).toHaveTitle(/kudarap/)
 })
