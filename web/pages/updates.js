6c175f317b4e70abe0b38a7c22e99f6bb0aab353
kudarap <goto@chiligarlic.com>
2026-08-19 23:09:48 +0800

    add steam id style highlight

--- web/pages/updates.js
+++ web/pages/updates.js
@@ -27,6 +27,34 @@ const releaseItem = (releaseDate, tag, label, contents) => ({
 })
 
 const releases = [
+  releaseItem('Aug 5, 2026', 'v0.25.4', 'Fixed PayPal and improved Phantasm crawler success rate', [
+    'added highlights and label for steam id #245',
+    'fixed subscription checker tool #243',
+    'fixed clearing of subscription expiration #244',
+    'fixed inject steam headers #248',
+  ]),
+  releaseItem('Aug 4, 2026', 'v0.25.3', 'Clean ups, Tips, and fuck Google Analytics', [
+    'added welcome dialog with scam-safety tips for new visitors #232',
+    'fixed clipped set images on item card and list rows #228',
+    'fixed page title #227',
+    'fixed nextjs migration lint warnings and errors #223',
+    'fixed docker image version tag and image hash #221',
+    'fixed sitemap rendering invalid meta data',
+    'removed google analytics because fuck google',
+    'removed steam rep links and contents #207',
+  ]),
+  releaseItem('Jul 31, 2026', 'v0.25.2', 'Hotfix', [
+    'fix incomplete URL substring sanitization on checkout page',
+  ]),
+  releaseItem('Jul 31, 2026', 'v0.25.1', 'Hotfix', ['fix unsanitized file path']),
+  releaseItem('Jul 31, 2026', 'v0.25.0', 'Nextjs v16 upgrade', [
+    'added uptime on version endpoint',
+    'upgraded nextjs 12 to 16',
+    'upgraded react 17 to 19',
+    'upgraded eslint 8 to 9',
+    'upgraded other ui dependecies',
+    'format go code for go1.26 replacing interface to any',
+  ]),
   releaseItem('Jun 25, 2026', 'v0.24.0', 'Dark Carnival', [
     'added dark carnival event theme',
     'added umami web analytics in deprecation of google analytics',
@@ -47,7 +75,7 @@ const releases = [
     'fixed config requiring .env file to exists',
   ]),
   releaseItem(
-    'March 10, 2026',
+    'Mar 10, 2026',
     'v0.23.2',
     'Clickhouse integration, new pages and code improvements',
     [
@@ -136,8 +164,7 @@ export default function Updates() {
                 <Typography color="secondary" component="span">
                   {release.tag}
                 </Typography>{' '}
-                {release.label}
-                <LaunchIcon fontSize="relative" />
+                {release.label} <LaunchIcon fontSize="relative" />
               </Link>
               <ul>
                 {release.contents.map(line => (
@@ -152,7 +179,7 @@ export default function Updates() {
           ))}
 
           <Typography variant="h5" gutterBottom>
-            June 25, 2025
+            Jun 25, 2025
           </Typography>
           <Link
             target="_blank"
@@ -215,7 +242,7 @@ export default function Updates() {
           <br />
 
           <Typography variant="h5" gutterBottom>
-            October 8, 2022
+            Oct 8, 2022
           </Typography>
           <Typography color="textSecondary">
             <ul>
@@ -240,7 +267,7 @@ export default function Updates() {
           <br />
 
           <Typography variant="h5" gutterBottom>
-            September 3, 2022
+            Sep 3, 2022
           </Typography>
           <Typography color="textSecondary">
             <ul>
